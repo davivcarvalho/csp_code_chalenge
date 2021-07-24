92ad1f517fceceec3a41bdbeb3dc5719ef111837
Davi Carvalho <caminhounico@gmail.com>
2021-07-24 11:47:43 -0300

    done edit contact usecase

--- src/useCases/contacts/editContact/editContact.dto.ts
+++ src/useCases/contacts/editContact/editContact.dto.ts
@@ -1,30 +1,15 @@
-import { IsEmail, IsNotEmpty, IsOptional, Length, ValidateNested } from 'class-validator'
-import { Type } from 'class-transformer'
-
-class Phone {
-  @Length(2, 2)
-  prefix: string
-
-  @Length(8, 9)
-  number: string
-}
+import { IsEmail, IsOptional, Length } from 'class-validator'
 
 export class EditContactDto {
   @IsOptional()
   @Length(2, 20)
-  firstName: string
+  firstName?: string
 
   @IsOptional()
   @Length(2, 20)
-  lastName: string
+  lastName?: string
 
   @IsOptional()
   @IsEmail()
-  email: string
-
-  @IsOptional()
-  @IsNotEmpty()
-  @ValidateNested()
-  @Type(() => Phone)
-  phones: Phone[]
+  email?: string
 }
