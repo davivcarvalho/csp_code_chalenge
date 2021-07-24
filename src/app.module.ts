d3578eece35e9501bec48e01f028af30d70a81c9
Davi Carvalho <caminhounico@gmail.com>
2021-07-23 22:12:04 -0300

    configure db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysqldb',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'csp',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
