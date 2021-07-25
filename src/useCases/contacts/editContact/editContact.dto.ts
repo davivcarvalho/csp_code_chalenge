5a2f31d4a1afb6dc02391acda4373548b1090b38
Davi Carvalho <caminhounico@gmail.com>
2021-07-25 09:29:15 -0300

    permit edit phones when update a contact

--- src/useCases/contacts/editContact/editContact.dto.ts
+++ src/useCases/contacts/editContact/editContact.dto.ts
@@ -1,4 +1,15 @@
-import { IsEmail, IsOptional, Length } from 'class-validator'
+import { Type } from 'class-transformer'
+import { ArrayNotEmpty, IsEmail, IsOptional, Length, ValidateNested } from 'class-validator'
+
+class Phone {
+  @IsOptional()
+  @Length(2, 2)
+  prefix: string
+
+  @IsOptional()
+  @Length(8, 9)
+  number: string
+}
 
 export class EditContactDto {
   @IsOptional()
@@ -12,4 +23,10 @@ export class EditContactDto {
   @IsOptional()
   @IsEmail()
   email?: string
+
+  @IsOptional()
+  @ArrayNotEmpty()
+  @ValidateNested()
+  @Type(() => Phone)
+  phones: Phone[]
 }
--- src/useCases/contacts/editContact/editContact.service.ts
+++ src/useCases/contacts/editContact/editContact.service.ts
@@ -1,23 +1,43 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Contact } from 'src/entities/contact.entity'
+import { Phone } from 'src/entities/phone.entity'
 import { Repository } from 'typeorm'
 import { EditContactDto } from './editContact.dto'
 
 @Injectable()
 export class EditContactService {
-  constructor(@InjectRepository(Contact) private contactsRepository: Repository<Contact>) {}
+  constructor(
+    @InjectRepository(Contact) private contactsRepository: Repository<Contact>,
+    @InjectRepository(Phone) private phonesRepository: Repository<Phone>
+  ) {}
 
   async editOne(id: string, data: EditContactDto) {
-    const contact = await this.contactsRepository.findOne(id)
+    const contact = await this.contactsRepository.findOne(id, { relations: ['phones'] })
     if (!contact) throw new HttpException('Not found a editable contact!', HttpStatus.BAD_REQUEST)
 
     if (data.email) contact.email = data.email
     if (data.firstName) contact.firstName = data.firstName
     if (data.lastName) contact.lastName = data.lastName
 
+    await this.editContactPhones(contact, data.phones as Phone[])
+
     await this.contactsRepository.save(contact)
 
-    return contact
+    return await this.contactsRepository.findOne(id, { relations: ['phones'] })
+  }
+
+  async editContactPhones(contact: Contact, givenPhones: Phone[]) {
+    const deletedPhones = contact.phones.filter(x => !givenPhones.some(y => y.id === x.id))
+    if (deletedPhones.length > 0) await this.phonesRepository.delete(deletedPhones.map(e => e.id))
+
+    const phonesToInsert = givenPhones.filter(e => !e.id)
+    if (phonesToInsert.length > 0) {
+      const insertedPhones = await this.phonesRepository.save(phonesToInsert)
+      contact.phones = contact.phones.concat(insertedPhones)
+    }
+
+    const phonesToEdit = givenPhones.filter(e => !!e.id)
+    if (phonesToEdit.length > 0) await this.phonesRepository.save(phonesToEdit)
   }
 }
